23d7c1dc6301ad1a26d6182b90a955fb15294df0
Felix Kebaki <felixkebaki@gmail.com>
2026-05-07 08:28:21 +0300

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`Listening to port ${PORT}.....`);
+});
